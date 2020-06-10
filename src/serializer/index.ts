4ade46040e27afe28b2c83d5080f1325d6feb41f
Johan Nordberg <its@johan-nordberg.com>
2020-06-10 10:46:50 +0200

    Expose ABI synthesizer

--- src/serializer/index.ts
+++ src/serializer/index.ts
@@ -1,7 +1,9 @@
 import {encode as abiEncode} from './encoder'
 import {decode as abiDecode} from './decoder'
+import {synthesizeABI} from './serializable'
 
 export namespace Serializer {
     export const encode = abiEncode
     export const decode = abiDecode
+    export const synthesize = synthesizeABI
 }
