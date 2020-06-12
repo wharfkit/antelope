ff8b1bc547e789bda377f46ec08bea174719d1b1
Johan Nordberg <its@johan-nordberg.com>
2020-06-12 08:47:43 +0200

    Unnecessary optimiaztions

--- src/utils.ts
+++ src/utils.ts
@@ -11,37 +11,47 @@ export function arrayEquals(a: ArrayLike<number>, b: ArrayLike<number>) {
     return true
 }
 
-let hexLookup: string[] | undefined
-export function arrayToHex(array: Uint8Array) {
-    if (!hexLookup) {
-        hexLookup = new Array<string>(0xff)
-        for (let i = 0; i <= 0xff; ++i) {
-            hexLookup[i] = i.toString(16).padStart(2, '0')
-        }
+const hexLookup: {enc?: Array<string>; dec?: Record<string, number>} = {}
+function buildHexLookup() {
+    hexLookup.enc = new Array<string>(0xff)
+    hexLookup.dec = {}
+    for (let i = 0; i <= 0xff; ++i) {
+        const b = i.toString(16).padStart(2, '0')
+        hexLookup.enc[i] = b
+        hexLookup.dec[b] = i
     }
-    const len = array.byteLength
+}
+
+export function arrayToHex(array: ArrayLike<number>) {
+    if (!hexLookup.enc) {
+        buildHexLookup()
+    }
+    const len = array.length
     const rv = new Array<string>(len)
-    for (let i = 0; i < array.byteLength; ++i) {
-        rv[i] = hexLookup[array[i]]
+    for (let i = 0; i < len; ++i) {
+        rv[i] = hexLookup.enc![array[i]]
     }
     return rv.join('')
 }
 
 export function hexToArray(hex: string) {
+    if (!hexLookup.dec) {
+        buildHexLookup()
+    }
     if (typeof hex !== 'string') {
         throw new Error('Expected string containing hex digits')
     }
     if (hex.length % 2) {
         throw new Error('Odd number of hex digits')
     }
-    const l = hex.length / 2
-    const result = new Uint8Array(l)
-    for (let i = 0; i < l; ++i) {
-        const x = parseInt(hex.substr(i * 2, 2), 16)
-        if (Number.isNaN(x)) {
+    const len = hex.length / 2
+    const result = new Uint8Array(len)
+    for (let i = 0; i < len; i++) {
+        const b = hexLookup.dec![hex[i * 2] + hex[i * 2 + 1]]
+        if (b === undefined) {
             throw new Error('Expected hex string')
         }
-        result[i] = x
+        result[i] = b
     }
     return result
 }
