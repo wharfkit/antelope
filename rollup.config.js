183d9e3883c4bd6bb41215591ab4a343876f96f2
Johan Nordberg <its@johan-nordberg.com>
2020-11-25 16:46:58 +0900

    Target es6 for common js bundle
    Also add license banner to generated files

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,19 @@
 import dts from 'rollup-plugin-dts'
 import esbuild from '@cush/rollup-plugin-esbuild' // https://github.com/egoist/rollup-plugin-esbuild/pull/138
+import fs from 'fs'
+
+const license = fs.readFileSync('LICENSE').toString('utf-8')
+
+const banner = `
+/**
+ * EOSIO Core by Greymass
+ * Library for working with EOSIO blockchains in JavaScript environments
+ * https://github.com/greymass/eosio-core
+ *
+ * @license
+ * ${license.replace(/\n/g, '\n * ')}
+ */
+`.trim()
 
 const name = require('./package.json').main.replace(/\.js$/, '')
 const ext = (format) => (format == 'dts' ? 'd.ts' : format == 'cjs' ? 'js' : 'mjs')
@@ -9,9 +23,16 @@ const bundle = (format) => ({
         file: `${name}.${ext(format)}`,
         format: format == 'cjs' ? 'cjs' : 'es',
         sourcemap: format != 'dts',
+        banner,
     },
-
-    plugins: format == 'dts' ? [dts()] : [esbuild()],
+    plugins:
+        format == 'dts'
+            ? [dts()]
+            : [
+                  esbuild({
+                      target: format == 'cjs' ? 'es2015' : 'esnext',
+                  }),
+              ],
     external: (id) => !/^[./]/.test(id),
     onwarn(warning, rollupWarn) {
         if (warning.code !== 'CIRCULAR_DEPENDENCY') {
