ea7ece3cb9153c2caf5e005a37d5c1068d9c8adb
Johan Nordberg <its@johan-nordberg.com>
2021-01-29 16:20:02 +0900

    Add some encoding helpers

--- src/serializer/index.ts
+++ src/serializer/index.ts
@@ -15,7 +15,16 @@ export type {
 export namespace Serializer {
     export const encode = abiEncode
     export const decode = abiDecode
+    /** Create an EOSIO ABI definition for given core type. */
     export function synthesize(type: ABISerializableConstructor) {
         return synthesizeABI(type).abi
+    }
+    /** Create JSON representation of a core object. */
+    export function stringify(object: any) {
+        return JSON.stringify(object)
+    }
+    /** Create a vanilla js representation of a core object. */
+    export function objectify(object: any) {
+        return JSON.parse(JSON.stringify(object))
     }
 }
